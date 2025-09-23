b03ecd0113e576b39bbd600731bbede7e9f0ee31
Potassiumzz <brainiacgamer444@gmail.com>
2025-09-23 16:53:34 +0545

    Create a delete feature for todos
    - Also create a complete button but it does not do anything useful yet.

--- src/app/components/TodoApp.tsx
+++ src/app/components/TodoApp.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import TodoInput from "./TodoInput";
+import { todo } from "node:test";
 
 export default function TodoApp() {
   const [todoList, setTodoList] = React.useState<string[]>([]);
@@ -19,6 +20,15 @@ export default function TodoApp() {
     setTodoList([...todoList, todo]);
   }
 
+  function handleComplete(todo: string) {
+    console.log(todo);
+  }
+
+  function handleDelete(index: number) {
+    setTodoList(todoList.filter((todo, i) => i != index));
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }
+
   return (
     <div className="px-8">
       <TodoInput addTodoList={addTodoList} />
@@ -27,8 +37,22 @@ export default function TodoApp() {
           todoList.length > 0 ?
             todoList.map((todo, index) => {
               return (
-                <div className="bg-gray-500/[.5] p-2 rounded-sm">
-                  {todo}
+                <div className="bg-gray-500/[.5] p-2 rounded-sm" key={index}>
+                  <div className="flex justify-between">
+                    {todo}
+                    <div className="flex gap-5">
+                      <button
+                        onClick={() => handleComplete(todo)}
+                        className="border px-2 hover:bg-white hover:text-black duration-200 cursor-pointer">
+                        Complete
+                      </button>
+                      <button
+                        onClick={() => handleDelete(index)}
+                        className="border px-2 hover:bg-red-600 duration-200 cursor-pointer">
+                        Delete
+                      </button>
+                    </div>
+                  </div>
                 </div>
               )
             }) :
